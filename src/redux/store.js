158f067b3b4e0d43d6a437d1d2d968efdbeed04f
Volodymyr Kurov <vokur13@gmail.com>
2022-12-01 18:55:21 +0200

    createSlice added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,43 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit'
 
 
-export const add = createAction('items/add');
-export const remove = createAction('items/remove');
+// export const add = createAction('items/add');
+// export const remove = createAction('items/remove');
 
-const itemsReducer = createReducer([], {
-    [add]: (state, action) => state + action.payload,
-    [remove]: (state, action) => state - action.payload,
-});
+
+// IMMER
+// const itemsReducer = createReducer([], {
+//     // [add]: (state, action) => [...state, action.payload],
+//     [add]: (state, action) => state.push(action.payload),
+//     // [remove]: (state, action) => [...state, action.payload],
+//     [remove]: (state, action) => state.filter(item=>item.id!==action.payload)
+// });
+
+// CreateSlice
+const itemsSlice = createSlice({
+    name: 'items',
+    initialState: [],
+    reducers: {
+        add(state, action) {
+            state.push(action.payload)
+        },
+        remove(state, action) {
+           return state.filter(item=>item.id!==action.payload)
+        }
+    }
+})
+
+export const { add, remove } = itemsSlice.actions;
+
+// export const store = configureStore({
+//     reducer: {
+//         items: itemsReducer
+//     },
+// });
 
 export const store = configureStore({
     reducer: {
-        items: itemsReducer
+        items: itemsSlice.reducer
     },
 });
